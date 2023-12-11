a923e9a227f97a3aff01d20c6e3457ae09d3b327
ShubhamBawankule25 <acebee26@gmail.com>
2023-12-11 08:54:01 +0530

    pycharm sync

--- Day5 HTTP API/Excersice_json_quiz_app.py
+++ Day5 HTTP API/Excersice_json_quiz_app.py
@@ -1,39 +1,62 @@
+# import pprint
 import requests
 import json
+import html
 import random
-# import pprint
 
-url = "https://opentdb.com/api.php?amount=1&category=27&difficulty=easy&type=multiple"
-
+# api
+url = "https://opentdb.com/api.php?amount=1"
+# to stop execution/ exit loop
 endgame = ""
+score_correct = 0
+score_incorrect = 0
 
+print("This program gives you a trivia")
+input("press any key if you are ready to play...." + "\n")
 
-
-print("This program gives you a trivia to play.")
-input("press any key if you are ready to play....")
-
-
-while endgame!= "quit":
-    
+while endgame != "quit":
+    opt = 1
     r = requests.get(url)
     if r.status_code != 200:
         endgame = input("Sorry something went wrong, press enter to try again, or type quit to end. ")
     else:
         quiz_data = json.loads(r.text)
+        # pprint.pprint(quiz_data)
+        # input("Press enter to get a new question")
+    data_val = False
+    question = quiz_data['results'][0]['question']
+    answers = quiz_data['results'][0]['incorrect_answers']
+    correct_ans = quiz_data['results'][0]['correct_answer']
+    answers.append(correct_ans)
+    random.shuffle(answers)
 
-    print(quiz_data['results'][0]['question'], ":")
+    print(html.unescape(question) + "\n")
     print("Options :")
-    
-    options = (quiz_data['results'][0]['incorrect_answers'][0], 
-          (quiz_data['results'][0]['correct_answer']) , ":")
-    
-    answer = (quiz_data['results'][0]['correct_answer'])
-    guess = input("Enter your answer: ")
-    if guess == answer:
-        print("Correct, thats the right answer")
-        break
-    else:
-        print("Wrong, the correct answer is" + (quiz_data['results'][0]['correct_answer']))
-        
-    
+    for answer in answers:
+        print(str(opt) + "-", html.unescape(answer))
+        opt += 1
 
+    # answer = correct_ans
+    # while data_val == False:
+        guess = input("\nEnter your answer number: ")
+        # try:
+        #     guess = int(guess)
+        # except:
+        #     print("input must be integer between options 1-4")
+    guess = answers[int(guess)-1]
+
+    if guess == correct_ans:
+        print("Correct, the right answer is " + correct_ans)
+        score_correct += 1
+    else:
+        print("Wrong, the correct answer is " + (quiz_data['results'][0]['correct_answer']))
+        score_incorrect += 1
+
+    print("\n######################################")
+    print("Your Score is :")
+    print("correct answers: " + str(score_correct), "\nincorrect answers: " + str(score_incorrect))
+    print("######################################\n")
+
+    endgame = input("Press enter if you want to try another question or  type quit to exit.")
+
+print("\n Thanks for playing.")
