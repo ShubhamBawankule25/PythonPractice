657b5354b0b2338494b005b50816b9d66f22d937
ShubhamBawankule25 <acebee26@gmail.com>
2023-12-27 23:45:35 +0530

    ..

---
+++ Day6/8.issues in classes.py
@@ -0,0 +1,27 @@
+
+
+class Phone:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        # self.price = max(price, 0)
+        if price > 0:               # code will not accept negative price
+            self.price = price
+        else:
+            self.price = 0
+        # self.allSpecs = f"{self.brand} {self.model} and price is Rs {self.price}"
+    
+    @property       #with this method behaves like attribute
+    def allSpecs(self):
+        return f"{self.brand} {self.model} and price is Rs {self.price}"
+    
+    def fullname(self):
+        return f"{self.brand}  {self.model}"
+    
+phone1 = Phone("Nokia", "6", -15000)
+
+
+print(phone1.fullname())
+
+print(phone1.allSpecs()) #<-- without @proprty
+print(phone1.allSpecs)  #<--- with @property
