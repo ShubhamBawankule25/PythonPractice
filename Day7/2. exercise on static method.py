d56551a920d5560a2647f26dab2910c51b0f441d
ShubhamBawankule25 <acebee26@gmail.com>
2024-01-08 16:07:14 +0530

    refactor

--- Day7/2. exercise on static method.py
+++ Day7/2. exercise on static method.py
@@ -8,6 +8,17 @@ class Date:         # constructor / init method
     def arrangeIntoDatePattern(self):   # instance method as it contains self as constraint
         date = f"{self.day}-{self.month}-{self.year}"
         return date
+    
+    @classmethod
+    def detDataFromString(cls, string):
+        import re
+        date = re.findall('\d{2}-\d{2}-\d{4} | \d{2}/\d{2}/\d{4}')[0]
+        listDate = date.replace('/', '-').split('-')
+        date = listDate[0]
+        month = listDate[1]
+        year = listDate[2]
+        return cls(date, month, year)
+        
         
 date1 = Date(2024, 0o1, 0o4)
 
